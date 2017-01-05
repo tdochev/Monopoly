bddc2e91c65e72fcddf9e136ac25291f3ebdbc0b
DESKTOP-HI8FKDS\tdochev <tddochev@gmail.com>
2017-01-05 08:52:56 +0200

    Add project files.

---
+++ Monopoly/Contracts/IProperty.cs
@@ -0,0 +1,18 @@
+﻿using Monopoly.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Contracts
+{
+    interface IProperty : ITile
+    {
+        int? Rent { get; }
+
+        PropertyType propertyType { get; }
+
+        Color color { get; }
+    }
+}
---
+++ Monopoly/Enums/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Enums
+{
+    public enum Color
+    {
+        Brown,
+        LightBlue,
+        Pink,
+        Orange,
+        Red,
+        Yellow,
+        Green,
+        DarkBlue
+    }
+}
